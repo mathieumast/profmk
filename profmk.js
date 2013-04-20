78149d53235eae9181faba29751e1fa5347265f9
mathieumast <mathieu.mast@gmail.com>
2013-04-20 13:56:02 +0200

    Add wait and timeout implementations

--- profmk.js
+++ profmk.js
@@ -1,251 +1,271 @@
 /**
- * Compact promise pattern implementation and more.
- * (https://github.com/mathieumast/profmk)
+ * Compact promise pattern implementation and more. (https://github.com/mathieumast/profmk)
  * 
- * Version : 0.5.3
+ * Version : 0.5.4
  * 
  * Copyright (c) 2013, Mathieu MAST
  * 
  * Licensed under the MIT license
  */
 (function() {
-  "use strict";
+    "use strict";
 
-  /**
-   * Returns true if object is an object.
-   */
-  var isObject = function(obj) {
-    return obj === Object(obj);
-  };
+    var profmk = {};
 
-  /**
-   * Returns true if object is an array.
-   */
-  var isArray = Array.isArray || function(obj) {
-    return toString.call(obj) == "[object Array]";
-  };
-
-  /**
-   * Returns the index of item in the array or -1 if item is not found.
-   */
-  var indexOf = Array.prototype.indexOf || function(array, item) {
-    var i = 0, l = array.length;
-    for (; i < l; i++)
-      if (array[i] === item)
-        return i;
-    return -1;
-  };
-
-  /**
-   * Slice object (array or arguments).
-   */
-  var slice = function(obj, start, end) {
-    return Array.prototype.slice.call(obj, start, end);
-  };
-
-  /**
-   * Extend object.
-   */
-  var extend = function(dest) {
-    var args = slice(arguments, 1), i = 0, l = args.length;
-    for (; i < l; i++) {
-      var source = args[i];
-      for ( var prop in source) {
-        dest[prop] = source[prop];
-      }
-    }
-    return dest;
-  };
-
-  /**
-   * Create a new instance of object and arguments.
-   */
-  var instantiate = function(Obj, args) {
-    var obj;
-    if (isArray(args)) {
-      switch (args.length) {
-      case 0:
-        obj = new Obj();
-        break;
-      case 1:
-        obj = new Obj(args[0]);
-        break;
-      case 2:
-        obj = new Obj(args[0], args[1]);
-        break;
-      case 3:
-        obj = new Obj(args[0], args[1], args[2]);
-        break;
-      case 4:
-        obj = new Obj(args[0], args[1], args[2], args[3]);
-        break;
-      case 5:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4]);
-        break;
-      case 6:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4], args[5]);
-        break;
-      case 7:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-        break;
-      case 8:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-        break;
-      case 9:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-        break;
-      case 10:
-        obj = new Obj(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-        break;
-      default:
-        throw new Error("Max arguments : 10");
-      }
-    } else {
-      obj = new Obj();
-    }
-    return obj;
-  };
-
-  /**
-   * Promise implementation.
-   */
-  var _Promise = function() {
-    var _callbacks = {
-      done : [],
-      fail : [],
-      progress : []
+    profmk.errors = {
+        timeout : "Timeout error"
     };
+
     /**
-     * Return copy of callbacks.
+     * Slice object (array or arguments).
      */
-    this.callbacks = function() {
-      return {
-        done : slice(_callbacks.done),
-        fail : slice(_callbacks.fail),
-        progress : slice(_callbacks.progress)
-      };
+    profmk.slice = function(obj, start, end) {
+        return Array.prototype.slice.call(obj, start, end);
     };
+
     /**
-     * Add handlers to be called when the Promise object is done, failed, or
-     * still in progress.
+     * Invoke function for context and many arguments and return promise.
      */
-    this.then = function(done, fail, progress) {
-      var addCallback = function(type, callback) {
-        if (typeof callback === "function") {
-          _callbacks[type].push(callback);
+    profmk.invoke = function(context, func) {
+        var res = func.apply(this, profmk.slice(arguments, 2));
+        return profmk.future().notifyDone(res).promise();
+    };
+
+    /**
+     * Returns the index of item in the array or -1 if item is not found.
+     */
+    profmk.indexOf = Array.prototype.indexOf || function(array, item) {
+        var i = 0, l = array.length;
+        for (; i < l; i++)
+            if (array[i] === item)
+                return i;
+        return -1;
+    };
+
+    /**
+     * Extend object.
+     */
+    profmk.extend = function(dest) {
+        var args = profmk.slice(arguments, 1), i = 0, l = args.length;
+        for (; i < l; i++) {
+            var source = args[i];
+            for ( var prop in source) {
+                dest[prop] = source[prop];
+            }
         }
-      };
-      addCallback("done", done);
-      addCallback("fail", fail);
-      addCallback("progress", progress);
-      return this;
+        return dest;
     };
-  };
 
-  /**
-   * Future implementation.
-   */
-  var _Future = function() {
-    var _step = "progress", _promise = new _Promise();
-    this._notify = function(type, array) {
-      if (_step === "progress") {
-        _step = type;
-        setTimeout(function() {
-          var _callbacks = _promise.callbacks();
-          for ( var i = 0; i < _callbacks[type].length; i++) {
-            var callback = _callbacks[type][i];
-            callback.apply(_promise, array);
-          }
-        }, 1);
-      }
-    };
     /**
-     * Return promise instance.
+     * Create a new instance of function and arguments.
      */
-    this.promise = function() {
-      return _promise;
-    };
-  };
-  /**
-   * Add handlers to be called when the Future object is done, failed, or still
-   * in progress.
-   */
-  _Future.prototype.then = function(done, fail, progress) {
-    this.promise().then(done, fail, progress);
-  };
-  /**
-   * Notify that the Future object is done.
-   */
-  _Future.prototype.notifyDone = function() {
-    this._notify("done", slice(arguments));
-  };
-  /**
-   * Notify that the Future object is failed.
-   */
-  _Future.prototype.notifyFail = function() {
-    this._notify("fail", slice(arguments));
-  };
-  /**
-   * Notify that the Future object is in progress.
-   */
-  _Future.prototype.notifyProgress = function() {
-    this._notify("progress", slice(arguments));
-  };
-
-  /**
-   * When implementation. When is an extention of Future.
-   */
-  var _When = function(promises) {
-    extend(this, new _Future());
-    var _results = [], _remaining = promises.length, self = this;
-    var watch = function(i, promise) {
-      promise.then(function(obj) {
-        _results[i] = obj;
-        if (--_remaining === 0) {
-          var joinObjs = [];
-          for ( var j = 0; j < _results.length; j++) {
-            joinObjs.push(_results[j]);
-          }
-          _Future.prototype.notifyDone.apply(self, joinObjs);
+    profmk.instantiate = function(func, args) {
+        if (profmk.isArray(args)) {
+            var obj;
+            switch (args.length) {
+            case 0:
+                obj = new func();
+                break;
+            case 1:
+                obj = new func(args[0]);
+                break;
+            case 2:
+                obj = new func(args[0], args[1]);
+                break;
+            default:
+                var q = [];
+                for ( var i = 0; i < args.length; i++) {
+                    q.push("args[" + i + "]");
+                }
+                obj = eval("new func(" + q.join(",") + ")");
+            }
+            return obj;
         } else {
-          self.notifyProgress(obj);
+            return new func();
         }
-      }, function(obj) {
-        self.notifyFail(obj);
-      }, function(obj) {
-        self.notifyProgress(obj);
-      });
     };
-    for ( var i = 0; i < promises.length; i++) {
-      watch(i, promises[i]);
-    }
-  };
-  extend(_When.prototype, _Future.prototype);
 
-  /**
-   * Export profmk.
-   */
-  var profmk = {};
-  profmk.isObject = isObject;
-  profmk.isArray = isArray;
-  profmk.indexOf = indexOf;
-  profmk.slice = slice;
-  profmk.extend = extend;
-  profmk.instantiate = instantiate;
-  profmk.future = function() {
-    return new _Future();
-  };
-  profmk.when = function() {
-    return new _When(slice(arguments));
-  };
-  if (typeof define === "function" && define.amd) {
-    define("profmk", function() {
-      return profmk;
-    });
-  } else if (typeof module !== "undefined" && module.exports) {
-    module.exports = profmk;
-  } else {
-    var root = this;
-    root["profmk"] = profmk;
-  }
+    /*
+     * Promise implementation.
+     */
+    var _Promise = function() {
+        var _callbacks = {
+            done : [],
+            fail : [],
+            progress : []
+        };
+        /*
+         * Return copy of callbacks.
+         */
+        this.callbacks = function() {
+            return {
+                done : profmk.slice(_callbacks.done),
+                fail : profmk.slice(_callbacks.fail),
+                progress : profmk.slice(_callbacks.progress)
+            };
+        };
+        /*
+         * Add handlers to be called when the Promise object is done, failed, or still in progress.
+         */
+        this.then = function(done, fail, progress) {
+            var addCallback = function(type, callback) {
+                if (typeof callback === "function") {
+                    _callbacks[type].push(callback);
+                }
+            };
+            addCallback("done", done);
+            addCallback("fail", fail);
+            addCallback("progress", progress);
+            return this;
+        };
+    };
+
+    /*
+     * Future implementation.
+     */
+    var _Future = function() {
+        var _step = "progress", _promise = new _Promise(), _ctx = this;
+        this._notify = function(type, array) {
+            if (_step === "progress") {
+                _step = type;
+                setTimeout(function() {
+                    var _callbacks = _promise.callbacks();
+                    for ( var i = 0; i < _callbacks[type].length; i++) {
+                        var callback = _callbacks[type][i];
+                        callback.apply(_ctx, array);
+                    }
+                }, 1);
+            }
+            return this;
+        };
+        /*
+         * Return promise instance.
+         */
+        this.promise = function() {
+            return _promise;
+        };
+    };
+    /*
+     * Add handlers to be called when the Future object is done, failed, or still in progress.
+     */
+    _Future.prototype.then = function(done, fail, progress) {
+        this.promise().then(done, fail, progress);
+    };
+    /*
+     * Notify that the Future object is done.
+     */
+    _Future.prototype.notifyDone = function() {
+        return this._notify("done", profmk.slice(arguments));
+    };
+    /*
+     * Notify that the Future object is failed.
+     */
+    _Future.prototype.notifyFail = function() {
+        return this._notify("fail", profmk.slice(arguments));
+    };
+    /*
+     * Notify that the Future object is in progress.
+     */
+    _Future.prototype.notifyProgress = function() {
+        return this._notify("progress", profmk.slice(arguments));
+    };
+    /**
+     * Get a new future object.
+     */
+    profmk.future = function() {
+        return new _Future();
+    };
+
+    /*
+     * When implementation.
+     */
+    var _When = function(objs) {
+        profmk.extend(this, new _Future());
+        var _results = [], _remaining = objs.length, _ctx = this;
+        for ( var i = 0; i < objs.length; i++) {
+            var elem = objs[i], promise;
+            if (profmk.isUndefined(elem) || profmk.isNull(elem)) {
+                promise = profmk.future().notifyDone(null).promise();
+            } else if (profmk.isFunction(elem.then)) {
+                promise = profmk.isFunction(elem.promise) ? elem.promise() : elem;
+            } else if (profmk.isFunction(elem)) {
+                var res = elem.apply(this, profmk.slice(arguments, 2));
+                promise = profmk.future().notifyDone(res).promise();
+            } else {
+                promise = profmk.future().notifyDone(elem).promise();
+                ;
+            }
+            profmk.invoke(_ctx, function(i, promise) {
+                promise.then(function(obj) {
+                    _results[i] = obj;
+                    if (--_remaining === 0) {
+                        _Future.prototype.notifyDone.apply(_ctx, _results);
+                    } else {
+                        _ctx.notifyProgress(obj);
+                    }
+                }, function(obj) {
+                    _ctx.notifyFail(obj);
+                }, function(obj) {
+                    _ctx.notifyProgress(obj);
+                });
+            }, i, promise);
+        }
+    };
+    profmk.extend(_When.prototype, _Future.prototype);
+    /**
+     * Get a new when promise object.
+     */
+    profmk.when = function() {
+        var when = new _When(profmk.slice(arguments));
+        return when.promise();
+    };
+
+    /**
+     * Get a new wait promise object.
+     */
+    profmk.wait = function(ms) {
+        var future = profmk.future(), objs = [ future ].concat(profmk.slice(arguments, 1));
+        var when = new _When(objs);
+        setTimeout(function() {
+            future.notifyDone(ms);
+        }, ms);
+        return when.promise();
+    };
+
+    /**
+     * Get a new timeout promise object.
+     */
+    profmk.timeout = function(ms) {
+        var when = new _When([ ms ].concat(profmk.slice(arguments, 1)));
+        setTimeout(function() {
+            when.notifyFail(profmk.errors.timeout);
+        }, ms);
+        return when.promise();
+    };
+
+    /*
+     * Function isArray, isObject, isFunction, isString, isBoolean, isNumber, isDate, isRegExp, isUndefined, isNull.
+     */
+    var types = [ "Array", "Object", "Function", "String", "Boolean", "Number", "Date", "RegExp", "Undefined", "Null" ];
+    for ( var i = 0; i < types.length; i++) {
+        profmk.invoke(profmk, function(type) {
+            profmk["is" + type] = function(obj) {
+                return toString.call(obj) == "[object " + type + "]";
+            };
+        }, types[i]);
+    }
+
+    /*
+     * Export profmk.
+     */
+    if (typeof define === "function" && define.amd) {
+        define("profmk", function() {
+            return profmk;
+        });
+    } else if (typeof module !== "undefined" && module.exports) {
+        module.exports = profmk;
+    } else {
+        var root = this;
+        root["profmk"] = profmk;
+    }
 }).call(this);
--- test/test.js
+++ test/test.js
@@ -4,177 +4,244 @@ var assert = require("chai").assert;
 
 describe("inherit with extend", function() {
 
-  var Parent = function() {
-    this.num = 6;
-    this.str = "string";
-  };
-  Parent.prototype.info = function() {
-    return this.num + " " + this.str;
-  };
+    var Parent = function() {
+        this.num = 6;
+        this.str = "string";
+    };
+    Parent.prototype.info = function() {
+        return this.num + " " + this.str;
+    };
 
-  var Child = function() {
-    profmk.extend(this, new Parent());
-  };
-  profmk.extend(Child.prototype, Parent.prototype);
+    var Child = function() {
+        profmk.extend(this, new Parent());
+    };
+    profmk.extend(Child.prototype, Parent.prototype);
 
-  it("inherit 1", function() {
-    var parent = new Parent();
-    expect(parent.info()).to.equal("6 string");
-    parent.num = 7;
-    parent.str = "toto";
-    expect(parent.info()).to.equal("7 toto");
+    it("inherit 1", function() {
+        var parent = new Parent();
+        expect(parent.info()).to.equal("6 string");
+        parent.num = 7;
+        parent.str = "toto";
+        expect(parent.info()).to.equal("7 toto");
 
-    var child = new Child();
-    expect(child.info()).to.equal("6 string");
-    parent.num = 9;
-    parent.str = "titi";
-    expect(parent.info()).to.equal("9 titi");
-  });
+        var child = new Child();
+        expect(child.info()).to.equal("6 string");
+        parent.num = 9;
+        parent.str = "titi";
+        expect(parent.info()).to.equal("9 titi");
+    });
 
-})
+});
 
 describe("future", function() {
 
-  it("progress & done", function(done) {
-    var future = profmk.future();
-    var progress = 0;
-    future.then(function(obj) {
-      expect(obj).to.equal("done");
-      expect(progress).to.equal(3);
-      done();
-    }, function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      expect(obj).to.equal("progress");
-      progress++;
+    it("progress & done", function(done) {
+        var future = profmk.future();
+        var progress = 0;
+        future.then(function(obj) {
+            expect(obj).to.equal("done");
+            expect(progress).to.equal(3);
+            done();
+        }, function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            expect(obj).to.equal("progress");
+            progress++;
+        });
+
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyDone("done");
     });
 
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyDone("done");
-  });
+    it("progress & fail", function(done) {
+        var future = profmk.future();
+        var progress = 0;
+        future.then(function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            expect(obj).to.equal("fail");
+            expect(progress).to.equal(3);
+            done();
+        }, function(obj) {
+            expect(obj).to.equal("progress");
+            progress++;
+        });
 
-  it("progress & fail", function(done) {
-    var future = profmk.future();
-    var progress = 0;
-    future.then(function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      expect(obj).to.equal("fail");
-      expect(progress).to.equal(3);
-      done();
-    }, function(obj) {
-      expect(obj).to.equal("progress");
-      progress++;
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyFail("fail");
     });
 
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyFail("fail");
-  });
+    it("done & !progress & !fail", function(done) {
+        var future = profmk.future();
+        var promise = future.promise();
+        promise.then(function(obj) {
+            setTimeout(done, 100);
+        }, function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            assert(false, "must not be called!");
+        });
 
-  it("done & !progress & !fail", function(done) {
-    var future = profmk.future();
-    var promise = future.promise();
-    promise.then(function(obj) {
-      setTimeout(done, 100);
-    }, function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      assert(false, "must not be called!");
+        future.notifyDone("done");
+        future.notifyProgress("progress");
+        future.notifyFail("fail");
     });
 
-    future.notifyDone("done");
-    future.notifyProgress("progress");
-    future.notifyFail("fail");
-  });
+    it("fail & !progress & !done", function(done) {
+        var future = profmk.future();
+        var promise = future.promise();
+        promise.then(function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            setTimeout(done, 100);
+        }, function(obj) {
+            assert(false, "must not be called!");
+        });
 
-  it("fail & !progress & !done", function(done) {
-    var future = profmk.future();
-    var promise = future.promise();
-    promise.then(function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      setTimeout(done, 100);
-    }, function(obj) {
-      assert(false, "must not be called!");
+        future.notifyFail("fail");
+        future.notifyProgress("progress");
+        future.notifyDone("done");
     });
-
-    future.notifyFail("fail");
-    future.notifyProgress("progress");
-    future.notifyDone("done");
-  });
 });
 
 describe("promise", function() {
 
-  it("progress & done", function(done) {
-    var future = profmk.future();
-    var promise = future.promise();
-    var progress = 0;
-    promise.then(function(obj) {
-      expect(obj).to.equal("done");
-      expect(progress).to.equal(3);
-      done();
-    }, function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      expect(obj).to.equal("progress");
-      progress++;
+    it("progress & done", function(done) {
+        var future = profmk.future();
+        var promise = future.promise();
+        var progress = 0;
+        promise.then(function(obj) {
+            expect(obj).to.equal("done");
+            expect(progress).to.equal(3);
+            done();
+        }, function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            expect(obj).to.equal("progress");
+            progress++;
+        });
+
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyDone("done");
     });
 
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyDone("done");
-  });
+    it("progress & fail", function(done) {
+        var future = profmk.future();
+        var promise = future.promise();
+        var progress = 0;
+        promise.then(function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            expect(obj).to.equal("fail");
+            expect(progress).to.equal(3);
+            done();
+        }, function(obj) {
+            expect(obj).to.equal("progress");
+            progress++;
+        });
 
-  it("progress & fail", function(done) {
-    var future = profmk.future();
-    var promise = future.promise();
-    var progress = 0;
-    promise.then(function(obj) {
-      assert(false, "must not be called!");
-    }, function(obj) {
-      expect(obj).to.equal("fail");
-      expect(progress).to.equal(3);
-      done();
-    }, function(obj) {
-      expect(obj).to.equal("progress");
-      progress++;
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyProgress("progress");
+        future.notifyFail("fail");
     });
-
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyProgress("progress");
-    future.notifyFail("fail");
-  });
 
 });
 
 describe("when", function() {
-  
-  it("progress & done", function(done) {
-    var future1 = profmk.future();
-    var future2 = profmk.future();
-    var future3 = profmk.future();
-    var when = profmk.when(future1, future2, future3);
-    var progress = 0;
-    when.then(function() {
-      expect(profmk.slice(arguments)).to.deep.equal(["done1", "done2", "done3"]);
-      expect(progress).to.equal(2);
-      done();
-    }, function(obj) {
-      assert(false, "must be called!");
-    }, function(obj) {
-      progress++;
+
+    it("progress & done", function(done) {
+        var future1 = profmk.future();
+        var future2 = profmk.future();
+        var future3 = profmk.future();
+        var when = profmk.when(future1, future2, future3, "string", 3, null, [ 1, 2, 3 ], {
+            a : 1,
+            b : 2
+        });
+        var progress = 0;
+        when.then(function() {
+            expect(profmk.slice(arguments)).to.deep.equal([ "done1", "done2", "done3", "string", 3, null, [ 1, 2, 3 ], {
+                a : 1,
+                b : 2
+            } ]);
+            expect(progress).to.equal(7);
+            done();
+        }, function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            progress++;
+        });
+
+        future1.notifyDone("done1");
+        future2.notifyDone("done2");
+        future3.notifyDone("done3");
     });
 
-    future1.notifyDone("done1");
-    future2.notifyDone("done2");
-    future3.notifyDone("done3");
-  });
-  
+});
+
+describe("wait", function() {
+
+    it("done", function(done) {
+        var when = profmk.wait(200);
+        when.then(function(obj) {
+            expect(obj).to.equal(200);
+            done();
+        }, function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            assert(false, "must not be called!");
+        });
+    });
+
+    it("progress & done", function(done) {
+        var when = profmk.wait(200, "string", 3);
+        var progress = 0;
+        when.then(function() {
+            expect(profmk.slice(arguments)).to.deep.equal([ 200, "string", 3 ]);
+            expect(progress).to.equal(2);
+            done();
+        }, function(obj) {
+            assert(false, "failed must be called!");
+        }, function(obj) {
+            progress++;
+        });
+    });
+
+});
+
+describe("timeout", function() {
+
+    it("progress & done", function(done) {
+        var when = profmk.timeout(200, "string", 3, profmk.wait(100));
+        var progress = 0;
+        when.then(function() {
+            expect(profmk.slice(arguments)).to.deep.equal([ 200, "string", 3, 100 ]);
+            expect(progress).to.equal(3);
+            done();
+        }, function(obj) {
+            assert(false, "failed must be called!");
+        }, function(obj) {
+            progress++;
+        });
+    });
+
+    it("fail", function(done) {
+        var when = profmk.timeout(200, profmk.wait(400));
+        var progress = 0;
+        when.then(function(obj) {
+            assert(false, "must not be called!");
+        }, function(obj) {
+            expect(obj).to.equal("Timeout error");
+            done();
+        }, function(obj) {
+            progress++;
+        });
+    });
+
 });
