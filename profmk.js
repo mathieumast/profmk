bab4f9565485eab1a016aac8e8479eeb0f47424f
Mathieu MAST <dev@sigvcompdev>
2014-04-08 13:47:37 +0200

    Change version to 7.4

--- profmk.js
+++ profmk.js
@@ -1,7 +1,7 @@
 /*
  * Compact promise pattern implementation and more. (https://github.com/mathieumast/profmk)
  * 
- * Version : 0.7.3
+ * Version : 0.7.4
  * 
  * Copyright (c) 2013, Mathieu MAST
  * 
